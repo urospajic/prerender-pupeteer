5e1c8cc573e51fe1746795f60fdca3d2c07951f9
urospajic <uros.pajic@codebehind.rs>
2019-04-05 15:10:19 +0200

    catch error if somethig went wrong and return 500 to the client

--- index.js
+++ index.js
@@ -2,23 +2,26 @@ const puppeteer = require('puppeteer');
 const express = require('express');
 
 const app = express();
+const waitTime = 5000;
 
 app.get('/prerender', async (req, res, next) => {
-  const html = await ssr(req.query.path);
-  // Add Server-Timing! See https://w3c.github.io/server-timing/.
-  res.set('Server-Timing', `Prerender;dur=888;desc="Headless render time (ms)"`);
-  return res.status(200).send(html); // Serve prerendered page as response.
+    try {
+        const html = await ssr(req.query.path);
+        res.set('Server-Timing', `Prerender;dur=${waitTime};desc="Headless render time (ms)"`);
+        return res.status(200).send(html);
+    } catch (err) {
+        return res.status(500);
+    }
 });
 
 app.listen(8080, () => console.log('Server started. Press Ctrl+C to quit'));
 
 async function ssr(url) {
-  const browser = await puppeteer.launch({headless: true});
-  const page = await browser.newPage();
-  // await page.goto(url, {waitUntil: 'networkidle0'});
-  await page.goto(url);
-  await page.waitFor(5000);
-  const html = await page.content(); // serialized HTML of page DOM.
-  await browser.close();
-  return html;
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitFor(waitTime);
+    const html = await page.content();
+    await browser.close();
+    return html;
 }
